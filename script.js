6664a93d00fed3a3349cd0a1a1e9743a5084e43f
Javon Gordon <Javonsito@Javonsitos-Macbook.local>
2023-02-22 22:28:00 -0500

    finished javascript

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ var ratings = document.getElementById("ratings");
 
 var cir = ratings.getElementsByClassName("circle");
 
-
-
     for (var i = 0; i < cir.length; i++) {
         cir[i].addEventListener("click", function() {
-          this.className.replace(" selected", "");
+
+          if(ratings.getElementsByClassName("circle selected").length != 0){
+            var current = ratings.getElementsByClassName("circle selected");
+            current[0].className = current[0].className.replace(" selected", "");
+          }
+         
           this.className += " selected";
+
         })
     
     }
